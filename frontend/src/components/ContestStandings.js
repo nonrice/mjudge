5deb6018749fecdcce7f4e223caa5ecd055ccb30
Eric Xue <widefin@gmail.com>
2025-06-18 20:48:40 -0400

    Basic leaderboard frontend

--- frontend/src/components/ContestStandings.js
+++ frontend/src/components/ContestStandings.js
@@ -1,4 +1,22 @@
+import useSWR from "swr";
+import { useParams } from "react-router-dom";
+import { getToken } from "../utils/auth";
+import { fetcher } from "../utils/fetcher";
+
+
 export default function ContestStandings() {
+  const { contestId } = useParams();
+  const { data, error, isLoading } = useSWR(
+    [`http://127.0.0.1:5001/api/contest/${contestId}/leaderboard`, {}],
+    ([url, options]) => fetcher(url, options)
+  )
+
+  if (isLoading) return <p>Loading standings...</p>;
+  if (error) return <p>Error loading standings: {error.message}</p>;
+
+  const sorted_legend = Object.keys(data.problem_legend)
+    .sort((a, b) => data.problem_legend[a] - data.problem_legend[b]);
+
   return (
     <div>
       <h2>Standings</h2>
@@ -6,21 +24,28 @@ export default function ContestStandings() {
         <thead>
           <tr>
             <th>Rank</th>
-            <th>Username</th>
+            <th>User</th>
+            <th>Solved</th>
             <th>Score</th>
+            {sorted_legend.map((problem, index) => (
+            <th key={index}>{problem}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>User1</td>
-            <td>100</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>User2</td>
-            <td>90</td>
-          </tr>
+          {data.entries.map((entry, index) => (
+            <tr key={entry.user_id}>
+              <td>{index + 1}</td>
+              <td>{entry.username}</td>
+              <td>{entry.solved.reduce((acc, curr) => acc + curr, 0)}</td>
+              <td>{entry.score}</td>
+              {sorted_legend.map((problem, problemIndex) => (
+                <td key={problemIndex}>
+                  {entry.attempts[problemIndex]}/{entry.solved[problemIndex] ? entry.times[problemIndex] : "--"}
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
